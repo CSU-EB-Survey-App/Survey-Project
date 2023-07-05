72c3bcab3c9d2141d7b3138fc2f769b43edea504
Domain2223 <leon.christian.cal@gmail.com>
2023-07-05 16:48:57 -0700

    Implemented the controller functions getRatings and createRatings

---
+++ controllers/rate.js
@@ -0,0 +1,15 @@
+
+
+
+
+// @desc  this controller will fetch all of the ratings tied to a poll
+// @route GET /api/v1/rating/
+exports.getRatings = asyncHandler(async (req, res, next) => {
+}); 
+
+
+    // @desc  this controller will create a rating and tie it to a poll
+// @route POST /api/v1/rating/
+exports.createRating = asyncHandler(async (req, res, next) => {
+    
+});
