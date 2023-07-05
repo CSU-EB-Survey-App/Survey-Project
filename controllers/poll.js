e9d41cc5c19b9aedb409168030ce5eff514b1a39
StephenDK <stephendklein08@gmail.com>
2023-07-05 16:03:09 -0700

    Get Poll Controller

---
+++ controllers/poll.js
@@ -0,0 +1,11 @@
+// -IMPORTS-
+const Polls = require("../models/Poll");
+
+
+// -Controllers-
+
+// @desc  Get a single poll
+// @route POST /api/v1/poll/:id
+exports.getPoll = asyncHandler(async (req, res, next) => {
+    
+});
--- server.js
+++ server.js
@@ -54,3 +54,4 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log("Server is Running");
 })
+
