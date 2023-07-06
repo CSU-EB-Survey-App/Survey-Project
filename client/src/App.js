bde47540e4be67e3144a9793d0d6582498fb2b86
StephenDK <stephendklein08@gmail.com>
2023-07-05 18:43:47 -0700

    React Testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
 
+
+import RegisterView from "./components/Register/register"
+import Account from "./components/Account/account"
+
 function App() {
   return (
     <div className="App">
       <p>Hello World</p>
+      <RegisterView />
+      <Account />
     </div>
   );
 }
---
+++ client/src/components/Account/account.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Register from '../Register/register'
+
+class Account extends Component {
+    render() {
+        return(
+            <Register />
+        );
+    }
+}
+
+export default Account
