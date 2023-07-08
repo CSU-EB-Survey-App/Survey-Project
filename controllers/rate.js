1f93666d8f525188691481b2b51192dbb722f7df
StephenDK <stephendklein08@gmail.com>
2023-07-08 15:59:12 -0700

    Create Rating Controller and Route

--- controllers/rate.js
+++ controllers/rate.js
@@ -13,6 +13,7 @@ exports.getRatings = asyncHandler(async (req, res, next) => {
     try {
         console.log("GETTING ALL RATINGS");
 
+        // Get all ratings from database
         const ratings = await Ratings.find({});
 
         // Send to client
@@ -35,12 +36,77 @@ exports.getRatings = asyncHandler(async (req, res, next) => {
 // @desc  this controller will create a rating and tie it to a poll
 // @route POST /api/v1/rating/
 exports.createRating = asyncHandler(async (req, res, next) => {
-    console.log("CREATING A RATING");
+    try {
+        console.log("CREATING A RATING");
 
-    res.status(200).json({
-        success: true
-    })
+        // Print request body to terminal
+        console.log("REQUEST BODY: ", req.body);
+
+        // Grab request body values
+        const { question, startDate, endDate, user } = req.body;
+
+        // Prepare and format data
+        const data = {
+            question,
+            startDate,
+            endDate,
+            user
+        }
+
+        // Print data to terminal
+        console.log("DATA: ", data);
+
+        // Creat rating in database
+        const rate = await Ratings.create(data);
+
+        // Send response back to client
+        res.status(200).json({
+            success: true,
+            rate
+        })
+    } catch(err) {
+        // Print error to terminal
+        console.log(err);
+
+        // Forward error to client
+        next(err);
+    }
 });
+
+/*
+ question: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    useful: {
+        type: Number,
+        default: 0
+    },
+    voters: {
+        type: Array
+    },
+    startDate: {
+        type: Date,
+    },
+    endDate: {
+        type: Date,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true
+    }
+*/
+
+
 
 
 // @desc  this controller will fetch a rating associated with a poll
