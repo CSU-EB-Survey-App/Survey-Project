64015a479f92a92e7f95faf4226f820a9ccffb7a
StephenDK <stephendklein08@gmail.com>
2023-07-15 01:54:10 -0700

    Authentication Unit Testing

--- __tests__/auth.test.ts
+++ __tests__/auth.test.ts
@@ -5,8 +5,10 @@ import supertest from "supertest";
 describe('Authentication', () => {
     describe("Get User Route", () => {
         describe("Given the user does not exist", () => {
-            it("Should return a 404", () => {
-                expect(true).toBe(true);
+            it("Should return a 404", async () => {
+                // expect(true).toBe(true); // Good way to test function setup
+                // const userID = "1234556";
+                // await supertest().get("api/v1/auth/user")
             })
         })
     })
