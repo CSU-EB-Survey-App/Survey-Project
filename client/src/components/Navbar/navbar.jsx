560c839920d2d48e42a9b3d47970bafb940fe4a5
StephenDK <stephendklein08@gmail.com>
2023-07-19 12:24:20 -0700

    Completed Dashboard and Fixed Component Errors

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,75 +1,72 @@
-import * as React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-
+import * as React from "react";
+import { useNavigate } from "react-router-dom";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
 const drawerWidth = 240;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
   ({ theme, open }) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: `-${drawerWidth}px`,
     ...(open && {
-      transition: theme.transitions.create('margin', {
+      transition: theme.transitions.create("margin", {
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
       marginLeft: 0,
     }),
-  }),
+  })
 );
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
+  transition: theme.transitions.create(["margin", "width"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
+  justifyContent: "flex-end",
 }));
 
-
-
-export default function Navbar() {
-  const navigate = useNavigate(); 
+export default function Navbar(props) {
+  const navigate = useNavigate();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
@@ -86,10 +83,10 @@ export default function Navbar() {
     localStorage.removeItem("token");
     // Redirect user to home page
     navigate("/");
-  }
+  };
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
         <Toolbar>
@@ -98,7 +95,7 @@ export default function Navbar() {
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
           >
             <MenuIcon />
           </IconButton>
@@ -111,9 +108,9 @@ export default function Navbar() {
         sx={{
           width: drawerWidth,
           flexShrink: 0,
-          '& .MuiDrawer-paper': {
+          "& .MuiDrawer-paper": {
             width: drawerWidth,
-            boxSizing: 'border-box',
+            boxSizing: "border-box",
           },
         }}
         variant="persistent"
@@ -125,7 +122,7 @@ export default function Navbar() {
             <ChevronLeftIcon />
           </IconButton>
         </DrawerHeader>
-        
+
         <List>
           {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
             <ListItem key={text} disablePadding>
@@ -134,39 +131,52 @@ export default function Navbar() {
               </ListItemButton>
             </ListItem>
           ))} */}
+          <ListItem key={"user"} disablePadding>
+            <ListItemButton
+              onClick={() => {
+                navigate("/user");
+                handleDrawerClose();
+              }}
+            >
+              <ListItemText primary={props.user.studentID} />
+            </ListItemButton>
+          </ListItem>
           <ListItem key={"dashboard"} disablePadding>
-              <ListItemButton onClick={() => {
+            <ListItemButton
+              onClick={() => {
                 navigate("/dashboard");
                 handleDrawerClose();
-              }}>
-                <ListItemText primary={"Dashboard"} />
-              </ListItemButton>
-            </ListItem>
-            <ListItem key={"search"} disablePadding>
-              <ListItemButton>
-                <ListItemText primary={"Search"} />
-              </ListItemButton>
-            </ListItem>
-            <ListItem key={"poll"} disablePadding>
-              <ListItemButton onClick={() => navigate("/new/poll")}>
-                <ListItemText primary={"Poll"} />
-              </ListItemButton>
-            </ListItem>
-            <ListItem key={"rating"} disablePadding>
-              <ListItemButton onClick={() => {
+              }}
+            >
+              <ListItemText primary={"Dashboard"} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem key={"search"} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={"Search"} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem key={"poll"} disablePadding>
+            <ListItemButton onClick={() => navigate("/new/poll")}>
+              <ListItemText primary={"Poll"} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem key={"rating"} disablePadding>
+            <ListItemButton
+              onClick={() => {
                 navigate("/new/rating");
                 handleDrawerClose();
-              }}>
-                <ListItemText primary={"Rating"} />
-              </ListItemButton>
-            </ListItem>
-            <ListItem key={"logout"} disablePadding>
-              <ListItemButton onClick={handleLogout}>
-                <ListItemText primary={"Logout"} />
-              </ListItemButton>
-            </ListItem>
+              }}
+            >
+              <ListItemText primary={"Rating"} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem key={"logout"} disablePadding>
+            <ListItemButton onClick={handleLogout}>
+              <ListItemText primary={"Logout"} />
+            </ListItemButton>
+          </ListItem>
         </List>
-        
       </Drawer>
     </Box>
   );
