27dfd8624e9142e79cb1b43d8d4e3505324742a3
StephenDK <stephendklein08@gmail.com>
2023-07-23 02:58:00 -0700

    URL Fix

--- client/src/components/PollDisplay/displayPoll.jsx
+++ client/src/components/PollDisplay/displayPoll.jsx
@@ -33,9 +33,12 @@ function DisplayPoll() {
   useEffect(() => {
     const fetchUser = async () => {
       let token = localStorage.getItem("token");
-      const user = await axios.post("http://localhost:8080/api/v1/auth/user", {
-        token,
-      });
+      const user = await axios.post(
+        "https://pioneerpolls-da615733ad68.herokuapp.com/api/v1/auth/user",
+        {
+          token,
+        }
+      );
       // console.log("USER: ", user);
       setUser({ ...user.data.user });
       setLoading(false);
@@ -43,7 +46,7 @@ function DisplayPoll() {
 
     const fetchPoll = async () => {
       const request = await axios.get(
-        `http://localhost:8080/api/v1/polls/${id}`
+        `https://pioneerpolls-da615733ad68.herokuapp.com/api/v1/polls/${id}`
       );
       console.log("POLL: ", request);
       setPoll({ ...request.data.poll });
@@ -60,7 +63,7 @@ function DisplayPoll() {
       console.log("ID: ", id);
       console.log("USER: ", user._id);
       let request = await axios.put(
-        `http://localhost:8080/api/v1/polls/answer/${id}`,
+        `https://pioneerpolls-da615733ad68.herokuapp.com/api/v1/polls/answer/${id}`,
         {
           answer: choice,
           user: user._id,
